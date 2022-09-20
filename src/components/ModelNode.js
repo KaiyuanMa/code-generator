d48b2cc984332737afffd108d2d0babc6da70e02
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-09-20 13:32:07 -0400

    added fucntion to change nodename, and relationship

--- src/components/ModelNode.js
+++ src/components/ModelNode.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 import { Handle, Position } from "react-flow-renderer";
 import { useDispatch, useSelector } from "react-redux";
 import ModelAttribute from "./ModelAttribute";
@@ -8,46 +8,82 @@ function ModelNode({ data }) {
   const [editModelName, setEditModelName] = useState(false);
   const dispatch = useDispatch();
   const { models } = useSelector((state) => state.models);
+
   let model = {};
   const modelId = data.modelId;
+
   if (models.length > 1) {
     model = models.find((model) => model.id == modelId);
   }
-  const [modelName, setModelName] = useState(model.name);
+  const [modelName, _setModelName] = useState(model.name);
+  const modelNameRef = useRef(modelName);
+  const setModelName = (data) => {
+    modelNameRef.current = data;
+    _setModelName(data);
+  };
 
   useEffect(() => {
     if (models.length > 1) {
       model = models.find((model) => model.id == modelId);
     }
-    console.log(model);
+    inputHelper();
   }, [models]);
 
   const handelClick = () => {
     dispatch(addModelEntry(modelId));
   };
 
-  const changeModelName = (ev) => {
+  const inputHelper = () => {
+    const modelForm = document.getElementById(`${modelId}-form`);
+    const modelInput = document.getElementById(`${modelId}-input`);
+    modelForm?.addEventListener("dblclick", function () {
+      modelInput.disabled = false;
+      modelInput.focus();
+      console.log(1);
+    });
+    modelInput?.addEventListener("blur", function () {
+      modelInput.disabled = true;
+      console.log(modelNameRef);
+      updateModelName();
+    });
+    modelInput?.addEventListener("keydown", (event) => {
+      if (event.isComposing || event.keyCode === 13) {
+        modelInput.disabled = true;
+      }
+    });
+    console.log(1);
+  };
+
+  const updateModelName = () => {
+    dispatch(editModelAC(modelId, { name: modelNameRef.current }));
+  };
+
+  const handelSubmit = (ev) => {
     ev.preventDefault();
-    setEditModelName(false);
-    dispatch(editModelAC(modelId, { name: modelName }));
+    updateModelName();
   };
 
   return models.length > 0 ? (
     <div className="model-node">
       <Handle type="target" position={Position.Top} />
       <div>
-        {!editModelName ? (
-          <p onClick={() => setEditModelName((prev) => !prev)}>{modelName}</p>
-        ) : (
-          <form action="" onSubmit={changeModelName}>
-            <input
-              type="text"
-              placeholder={model.name}
-              value={modelName}
-              onChange={(ev) => setModelName(ev.target.value)}
-            />
-          </form>
-        )}
+        <div className="model-node-drag" />
+        <form
+          className="model-name-form"
+          id={`${modelId}-form`}
+          onSubmit={handelSubmit}
+        >
+          <input
+            type="text"
+            className="model-name-input"
+            id={`${modelId}-input`}
+            placeholder={model.name}
+            value={modelName}
+            disabled={true}
+            onChange={(ev) => setModelName(ev.target.value)}
+          />
+          <p>{modelName}</p>
+        </form>
         {model.entries
           ? model.entries.map((entry) => <ModelAttribute entry={entry} />)
           : null}
