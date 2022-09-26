6f4115e6a4cb6c8089daa1c6fbf96e9d8ea142a0
Kaiyuan Ma <frankmkmk1@gmail.com>
2022-09-26 16:28:49 -0400

    added type

--- src/components/ModelAttribute.js
+++ src/components/ModelAttribute.js
@@ -20,16 +20,23 @@ function ModelAttribute(prop) {
     _setName(data);
   };
 
-  const inputHelper = () => {
-    const entryForm = document.getElementById(`${entryId}-form`);
-    const entryInput = document.getElementById(`${entryId}-input`);
+  const [type, _setType] = useState(entry.type);
+  const typeRef = useRef(type);
+  const setType = (data) => {
+    typeRef.current = data;
+    _setType(data);
+  };
+
+  const inputHelper = (from, input) => {
+    const entryForm = document.getElementById(`${entryId}-${from}`);
+    const entryInput = document.getElementById(`${entryId}-${input}`);
     entryForm?.addEventListener("dblclick", function () {
       entryInput.disabled = false;
       entryInput.focus();
     });
     entryInput?.addEventListener("blur", function () {
       entryInput.disabled = true;
-      updateEntryName();
+      updateEntry(input);
     });
     entryInput?.addEventListener("keydown", (event) => {
       if (event.isComposing || event.keyCode === 13) {
@@ -38,13 +45,11 @@ function ModelAttribute(prop) {
     });
   };
 
-  const updateEntryName = () => {
-    dispatch(updateModelEntryAC(modelId, entryId, { name: nameRef.current }));
-  };
-
-  const handelSubmit = (e) => {
-    e.preventDefault();
-    updateEntryName();
+  const updateEntry = (property) => {
+    if (property == "input")
+      dispatch(updateModelEntryAC(modelId, entryId, { name: nameRef.current }));
+    else if (property == "type")
+      dispatch(updateModelEntryAC(modelId, entryId, { type: typeRef.current }));
   };
 
   const showEntryContent = () => {
@@ -67,7 +72,8 @@ function ModelAttribute(prop) {
   };
 
   useEffect(() => {
-    inputHelper();
+    inputHelper("input-form", "input");
+    inputHelper("type-form", "type");
   }, []);
 
   useEffect(() => {
@@ -89,8 +95,7 @@ function ModelAttribute(prop) {
       <div className="model-attribute-header">
         <div className="model-attribute-header-content">
           <form
-            id={`${entryId}-form`}
-            onSubmit={handelSubmit}
+            id={`${entryId}-input-form`}
             className="model-attribute-header-content-form"
           >
             <input
@@ -103,6 +108,47 @@ function ModelAttribute(prop) {
             />
           </form>
           <div className="model-attribute-header-btns">
+            <form id={`${entryId}-type-form`} className="model-type-form">
+              <input
+                value={type}
+                placeholder="Enter or select type"
+                id={`${entryId}-type`}
+                onChange={(e) => setType(e.target.value)}
+                disabled={true}
+                className="model-type-input"
+              ></input>
+              <select
+                onChange={(e) => {
+                  setType(e.target.value);
+                  updateEntry("type");
+                }}
+                className="model-type-select"
+              >
+                <option defaultChecked></option>
+                <option value="Sequelize.STRING">Sequelize.STRING</option>
+                <option value="Sequelize.TEXT">Sequelize.TEXT</option>
+                <option value="Sequelize.BOOLEAN">Sequelize.BOOLEAN</option>
+                <option value="Sequelize.INTEGER">Sequelize.INTEGER</option>
+                <option value="Sequelize.UUID">Sequelize.UUID</option>
+                <option value="Sequelize.JSON">Sequelize.JSON</option>
+                <option value="Sequelize.STRING(100)">
+                  Sequelize.STRING(100)
+                </option>
+                <option value="Sequelize.STRING.BINARY">
+                  Sequelize.STRING.BINARY
+                </option>
+                <option value="Sequelize.CHAR">Sequelize.CHAR</option>
+                <option value="Sequelize.CHAR(100)">Sequelize.CHAR(100)</option>
+                <option value="Sequelize.CHAR.BINARY">
+                  Sequelize.CHAR.BINARY
+                </option>
+                <option value="Sequelize.CITEXT">Sequelize.CITEXT</option>
+                <option value="Sequelize.TSVECTOR">Sequelize.TSVECTOR</option>
+                <option value="Sequelize.SMALLINT">Sequelize.SMALLINT</option>
+                <option value="Sequelize.BIGINT">Sequelize.BIGINT</option>
+                <option value="Sequelize.JSONB">Sequelize.JSONB</option>
+              </select>
+            </form>
             {name == "id" ? (
               <div>
                 <button
